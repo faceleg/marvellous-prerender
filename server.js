f6dff9f69ace651c3471b9d8c11f693e28cddbdb
Michael Robinson <michael.robinson@solnet.co.nz>
2017-05-17 11:23:36 +1200

    (debug) log requests

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
-var prerender = require('./lib');
+var prerender = require('./lib')
 
 var server = prerender({
-    workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
-});
+  workers: process.env.PRERENDER_NUM_WORKERS,
+  iterations: process.env.PRERENDER_NUM_ITERATIONS,
+  logRequests: true
+})
 
 
-server.use(prerender.sendPrerenderHeader());
+server.use(prerender.sendPrerenderHeader())
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-server.use(prerender.blacklist());
+server.use(prerender.blacklist())
 // server.use(prerender.logger());
-server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
+server.use(prerender.removeScriptTags())
+server.use(prerender.httpHeaders())
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
-server.use(require('prerender-mongo'));
+server.use(require('prerender-mongo'))
 
-server.start();
+server.start()
